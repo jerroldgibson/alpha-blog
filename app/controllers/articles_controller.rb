680f8b49aba7c57f3436669644d61e2d41003d75
Jerrold Gibson <jerroldgibson@gmail.com>
2016-05-19 11:51:56 -0400

    Styled form, flash, and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was saved successfully!"
+      flash[:success] = "Article was saved successfully!"
       redirect_to article_path(@article)
     else
       render "new"
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully!"
+      flash[:success] = "Article was updated successfully!"
       redirect_to article_path(@article)
     else
       render "edit"
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was successfully deleted!"
+    flash[:danger] = "Article was successfully deleted!"
     redirect_to articles_path
   end
 
